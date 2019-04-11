324760c0ed9315c7030a9be844fcad95666e4280
dusan <dusanzoric6@gmail.com>
2019-04-11 08:40:19 +0100

    added DriverFactory

--- src/test/java/com/masteringselenium/BasicTest.java
+++ src/test/java/com/masteringselenium/BasicTest.java
@@ -13,31 +13,26 @@ public class BasicTest {
     private void googleExampleThatSearchesFor(final String searchString) {
 
         WebDriver driver = new FirefoxDriver();
-
         driver.get("http://www.google.com");
-
         WebElement searchField = driver.findElement(By.name("q"));
-
         searchField.clear();
         searchField.sendKeys(searchString);
-
         System.out.println("Page title is: " + driver.getTitle());
-
         searchField.submit();
-
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver driverObject) {
-                return driverObject.getTitle().toLowerCase().startsWith(searchString.toLowerCase());}
+                return driverObject.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+            }
         });
-
         System.out.println("Page title is: " + driver.getTitle());
-
         driver.quit();
     }
+
 
     @Test
     public void googleCheeseExample() {
         googleExampleThatSearchesFor("Cheese!");
+
     }
 
     @Test
