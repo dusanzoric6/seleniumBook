55b725dd6765423dbe37abecedec8924f52834b9
dusanzoric6@gmail.com <dusanzoric6@gmail.com>
2019-06-19 09:37:24 +0100

    added DriverType

--- src/test/java/com/masteringselenium/WebDriverThread.java
+++ src/test/java/com/masteringselenium/WebDriverThread.java
@@ -1,25 +1,30 @@
 package com.masteringselenium;
 
+import com.masteringselenium.config.DriverType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+
+import static com.masteringselenium.config.DriverType.FIREFOX;
+
 public class WebDriverThread {
 
     private WebDriver webdriver;
+    private DriverType selectedDriverType;
 
+    private final DriverType defaultDriverType = FIREFOX;
+    private final String browser = System.getProperty("browser").toUpperCase();
     private final String operatingSystem = System.getProperty("os.name").toUpperCase();
     private final String systemArchitecture = System.getProperty("os.arch");
 
     public WebDriver getDriver() throws Exception {
         if (webdriver == null) {
-            System.out.println(" ");
-            System.out.println("Current Operating System: " + operatingSystem);
-            System.out.println("Current Architecture: " + systemArchitecture);
-            System.out.println("Current Browser Selection: Firefox");
-            System.out.println(" ");
-            webdriver = new FirefoxDriver(new FirefoxOptions());
+            selectedDriverType = determineEffectiveDriverType();
+            DesiredCapabilities desiredCapabilities = selectedDriverType.getDesiredCapabilities();
+            instantiateWebDriver(desiredCapabilities);
         }
 
         return webdriver;
@@ -30,5 +35,25 @@ public class WebDriverThread {
             webdriver.quit();
             webdriver = null;
         }
+    }
+
+    private DriverType determineEffectiveDriverType() {
+        DriverType driverType = defaultDriverType;
+        try {
+            driverType = DriverType.valueOf(browser);
+        } catch (IllegalArgumentException ignored) {
+            System.err.println("Unknown driver specified, defaulting to '" + driverType + "'...");
+        } catch (NullPointerException ignored) {
+            System.err.println("No driver specified, defaulting to '" + driverType + "'...");
+        }
+        return driverType;
+    }
+
+    private void instantiateWebDriver(DesiredCapabilities desiredCapabilities) throws MalformedURLException {
+        System.out.println(" ");
+        System.out.println("Current Operating System: " + operatingSystem);
+        System.out.println("Current Architecture: " + systemArchitecture);
+        System.out.println("Current Browser Selection: " + selectedDriverType);System.out.println(" ");
+        webdriver = selectedDriverType.getWebDriverObject(desiredCapabilities);
     }
 }
